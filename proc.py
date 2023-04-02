fd62c18b6759ca0f823c135795c20aff3fa3a819
Dudifer <nyberg.jacob12@hotmail.com>
2023-04-02 14:18:52 -0500

    finished algortihm
    Finished the algo, added a simple runtime analysis function

--- proc.py
+++ proc.py
@@ -1,4 +1,7 @@
 import time
+import random
+import matplotlib.pyplot as plt
+import string
 def LCS(s, t):
     ti=time.time()
     ss,st=set(),set()
@@ -18,10 +21,37 @@ def LCS(s, t):
             lcs=s
     tf=time.time()
     return lcs, tf-ti
+def runtimeAnalysis():
+    def stringGenerator(n):
+        sl1,sl2=[],[]
+        for i in range(1,n+1):
+            s1,s2="",""
+            for j in range(1,i+1):
+                s1+=random.choice(string.ascii_letters)
+                s2+=random.choice(string.ascii_letters)
+            sl1.append(s1)
+            sl2.append(s2)
+        return sl1,sl2
 
+    n=int(input("Input length limit: "))
+    x=list(range(1,n+1))
+    strings1,strings2=stringGenerator(n)
+    y=[]
+    for i in range(n):
+        lcs, rt=LCS(strings1[i],strings2[i])
+        y.append(rt)
+    plt.plot(x,y)
+    plt.xlabel("String Lengths")
+    plt.ylabel("Runtimes")
+    plt.show()
 
-str1=input("String 1: ")
-str2=input("String 2: ")
-lcs, rt=LCS(str1, str2)
-print("LCS: "+lcs+"\nRuntime:",rt)
-
+def main():
+    inp=int(input("1=single lcs. 2=lcs runtime analysis.\n"))
+    if inp==1:
+        str1=input("String 1: ")
+        str2=input("String 2: ")
+        lcs, rt=LCS(str1, str2)
+        print("LCS: "+lcs+"\nRuntime:",rt)
+    if inp==2:
+        runtimeAnalysis()
+main()
